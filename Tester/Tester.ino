f3bfd6e25e360aabe5731058eae8e6f3dc0596d2
unknown <michael.a.feuerstein@gmail.com>
2018-12-20 00:26:43 -0500

    Finished maze

--- Tester/Tester.ino
+++ Tester/Tester.ino
@@ -5,13 +5,13 @@
 
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(196, PIN, NEO_GRB + NEO_KHZ800);
 
-int maze[4][7][7];
-int player = (0,0,0);
+uint32_t maze[4][7][7];
 int y = 0, z = 1, x = 1;
-
+uint32_t prev = 0;
+ 
 void setup() {
   ring.begin();
-  ring.setBrightness(32);
+  ring.setBrightness(80);
   ring.clear(); // clear all pixels
   ring.show();  // show all pixels
   for(int layer = 0; layer < 4; layer++){
@@ -29,9 +29,8 @@ void setup() {
   maze[0][4][0] = maze[0][4][1] = maze[0][4][2] =                 maze[0][4][4]                 = maze[0][4][6] =
   maze[0][5][0] = maze[0][5][1] = maze[0][5][2] =                 maze[0][5][4]                 = maze[0][5][6] =
   maze[0][6][0] = maze[0][6][1] = maze[0][6][2] = maze[0][6][3] = maze[0][6][4] = maze[0][6][5] = maze[0][6][6] =            
-                                                                                  
-  
-  
+
+                                                                                 
   maze[1][0][0] = maze[1][0][1] = maze[1][0][2] = maze[1][0][3] = maze[1][0][4] = maze[1][0][5] = maze[1][0][6] = 
   maze[1][1][0] =                                 
   maze[1][2][0] =                 maze[1][2][2] = maze[1][2][3] = maze[1][2][4] = maze[1][2][5] =
@@ -48,27 +47,32 @@ void setup() {
   maze[2][4][0] = maze[2][4][1] = maze[2][4][2] =                 maze[2][4][4] = maze[2][4][5] =
   maze[2][5][0] =                                                 maze[2][5][4] = 
   maze[2][6][0] = maze[2][6][1] = maze[2][6][2] = maze[2][6][3] = maze[2][6][4] = maze[2][6][5] = maze[2][6][6] = 
+
                   
-  
-  
                   maze[3][0][1] = maze[3][0][2] = maze[3][0][3] =                 maze[3][0][5] = maze[3][0][6] =
                                                                                                
   maze[3][2][0] = maze[3][2][1] =                 maze[3][2][3] = maze[3][2][4] = maze[3][2][5] = 
   maze[3][3][0] =                                 maze[3][3][3] = maze[3][3][4] = maze[3][3][5] = maze[3][3][6] = 
   maze[3][4][0] =                 maze[3][4][2] = maze[3][4][3] = maze[3][4][4] = maze[3][4][5] = maze[3][4][6] =
   maze[3][5][0] =                 maze[3][5][2] = maze[3][5][3] = maze[3][5][4] =                 maze[3][5][6] = 
-  maze[3][6][0] = maze[3][6][1] = maze[3][6][2] =                                                 maze[3][6][6] =                 
+  maze[3][6][0] = maze[3][6][1] = maze[3][6][2] =                                                 maze[3][6][6] = 0x0000FF;
 
+  // ups
+  maze[0][5][3] = maze[0][5][5] = maze[1][2][1] = maze[2][5][1] = maze[2][0][4] = maze[2][2][6] = maze[2][5][5] = 0xFF0000;
 
-  0x0000FF;
-    
-  maze[y][z][x] = 0x00F00F;
+  // down
+  maze[1][5][3] = maze[1][5][5] = maze[2][2][1] =  maze[3][5][1] = maze[3][0][4] = maze[3][2][6] = maze[3][5][5] = 0x00FF00;
+
+  // goal
+  maze[3][6][3] = 0xFFFFFF;
+  
+  maze[y][z][x] = 0xFFFF00;
     
   setMaze(maze);
   Serial.begin(9600);
 }
 
-void wipe(unsigned long color) {
+void wipe(uint32_t  color) {
   for(int i=49;i<ring.numPixels()/2;i++) {
     ring.setPixelColor(i, color);
     ring.show();
@@ -85,7 +89,7 @@ void setCoord(int y, int z, int x){
   ring.show();
 }
 
-void setCoord(int y, int z, int x, unsigned long color){
+void setCoord(int y, int z, int x, uint32_t  color){
   int layer = 49 * y;
   int row = row = (y%2?6 - z:z) * 7;
   int column = (y%2?(z%2?x:6 - x):(z%2?6 - x:x));
@@ -93,17 +97,17 @@ void setCoord(int y, int z, int x, unsigned long color){
   ring.setPixelColor(i, color);
 }
 
-void setMaze(int m[][7][7]){
-  unsigned color;
+void setMaze(uint32_t m[][7][7]){
+  uint32_t color;
   for(int layer = 0; layer < 4; layer++){
     for(int row = 0; row < 7; row++){
       for(int column = 0; column < 7; column++){
 //        if(layer > y - 2 && layer < y + 2 )
-        if(layer == y)
+        if(layer == y) //|| isNeighbor(layer, row, column))
           color =  m[layer][row][column];
         else
           color = 0;
-        setCoord(layer, row, column,color);
+        setCoord(layer, row, column, color);
       }
     }
   }
@@ -119,16 +123,12 @@ void setLayer(int y){
   }
 }
 
-void getNeighbors(int y, int z, int x){
-  for(int layer = y - 1; (layer < y + 2 || layer == 4); layer++){
-    for(int row = z - 1; (row < z + 2 || row == 7); row++){
-      for(int column = x - 1; (column < x + 2 || column == 7); column++){
-        if(layer > -1 && row > -1 && column > -1)
-          if(!(layer == y && row == z && column == x))
-            setCoord(layer, row, column);
-      }
-    }
-  }
+bool isNeighbor(int layer, int row, int column){
+  if( (layer == y || layer == y + 1 || layer == y - 1) &&
+      (row == z || row == z + 1 || row == z - 1) &&
+      (column == x || column == x + 1 || column == x - 1))
+     return true;
+  return false;
 }
 
 void loop() {
@@ -173,7 +173,7 @@ void rainbowCycle() {
       ring.setPixelColor(i, Wheel(((i * 256 / ring.numPixels()) + j) & 255));
     }
     ring.show();
-    delay(20);
+    delay(10);
   }
 }
 
@@ -192,7 +192,7 @@ uint32_t Wheel(byte WheelPos) {
   return ring.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
-boolean hitWall(int y, int z, int x, int m[][7][7]){
+boolean hitWall(int y, int z, int x, uint32_t m[][7][7]){
   return m[y][z][x] == 0x0000FF;
 }
 
@@ -205,52 +205,58 @@ void movePlayer(){
   
   if(receivedChar=='l'){
     if(!hitWall(y, z-1, x, maze) && z > 0){
-      setCoord(y, z, x, 0x00000);
-      maze[y][z][x] = 0x000000;
+      maze[y][z][x] = prev;
       z=z-1;
+      prev = maze[y][z][x];
     }
   }
   else if(receivedChar=='r'){
     if(!hitWall(y, z+1, x, maze) && z < 6){
-      setCoord(y, z, x, 0x00000);
-      maze[y][z][x] = 0x000000;
+      maze[y][z][x] = prev;
       z=z+1;
+      prev = maze[y][z][x];
     }
   }
   else if(receivedChar=='f'){
     if(!hitWall(y, z, x+1, maze) && x < 6){
-      setCoord(y, z, x, 0x00000);
-      maze[y][z][x] = 0x000000;
+      maze[y][z][x] = prev;
       x=x+1;
+      prev = maze[y][z][x];
     }
   }
   else if(receivedChar=='b'){
     if(!hitWall(y, z, x-1, maze) && x > 0){
-      setCoord(y, z, x, 0x00000);
-      maze[y][z][x] = 0x000000;
+      maze[y][z][x] = prev;
       x=x-1;
+      prev = maze[y][z][x];
     }
   }
   else if(receivedChar=='u'){
     if(!hitWall(y+1, z, x, maze) && y < 3){
-      setCoord(y, z, x, 0x00000);
-      maze[y][z][x] = 0x000000;
-      y=y+1;
-      yChange = true;
+      if(prev == 0xFF0000){
+        maze[y][z][x] = prev;
+        y=y+1;
+        prev = maze[y][z][x];
+      }
     }
   }
   else if(receivedChar=='d'){
     if(!hitWall(y-1, z, x, maze) && y > 0){
-      setCoord(y, z, x, 0x00000);
-      maze[y][z][x] = 0x000000;
-      y=y-1;
-      yChange = true;
+      if(prev == 0x00FF00){
+        maze[y][z][x] = prev;
+        y=y-1;
+        prev = maze[y][z][x];
+      }
     }
   }
-  maze[y][z][x] = 0x00F00F;
+  maze[y][z][x] = 0xFFFF00;
  }
- setCoord(y, z, x, 0x00F00F);
- if(yChange == true)
+ //setCoord(y, z, x, 0x00F00F);
+
+ if(prev == 0xFFFFFF)
+  rainbowCycle();
+ 
+ else
   setMaze(maze);
  ring.show();
 }
